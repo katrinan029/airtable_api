4d6bb7db1e0b6f4e5a0212d6b9a5c192a91b1d5a
Katrina Nguyen <katrinan@usc.edu>
2021-06-10 14:18:03 -0700

    improving ui

--- airtable-business-hours/app.js
+++ airtable-business-hours/app.js
@@ -1,13 +1,25 @@
 const base = require("airtable").base("appVwoybPzKlAVvbN");
+const express = require("express");
+const path = require("path");
 
-(async () => {
-  const records = await base("Business Hours")
-    .select({
-      view: "Grid view",
+const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "."));
+
+app.get("/", (req, res) => {
+  (async () => {
+    const records = await base("Business Hours")
+      .select({
+        view: "Grid view",
+      })
+      .firstPage()
+    
+    res.render("page", {
+      records,
     })
-    .firstPage()
-  
-  for (const record of records) {
-    console.log(record.fields)
-  }
-})()
+  })();
+
+});
+
+app.listen(3000, () => console.log("Server ready"))
+
